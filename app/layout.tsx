2ecb59d71421cf476d1be02c51bf19a1ee5a5cb7
Fattah Maulana <54757764+fattahmaulana@users.noreply.github.com>
2026-03-03 17:00:04 +0700

    Add Google Fonts preconnect and stylesheet links

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="id">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;1,9..40,300&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className="antialiased">
         {children}
       </body>
